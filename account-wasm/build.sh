632ad1de0be96428a75dbdeb39181d878ea08ed7
Tarrence van As <tarrence@vanas.family>
2025-05-13 14:21:52 -0400

    Rename session wasm

--- account-wasm/build.sh
+++ account-wasm/build.sh
@@ -3,21 +3,24 @@
 set -ex
 
 # Ensure wasm-opt is installed
-if ! command -v wasm-opt &> /dev/null; then
+if
+    ! command -v wasm-opt &
+    >/dev/null
+then
     echo "Installing wasm-opt..."
     npm install -g wasm-opt
 fi
 
 # Build account bundle
 RUSTFLAGS='-C link-arg=-s -C opt-level=z -C codegen-units=1' \
-wasm-pack build --target bundler --out-dir ./pkg-controller --release --features "controller_account,wee_alloc"
+    wasm-pack build --target bundler --out-dir ./pkg-controller --release --features "controller_account,wee_alloc"
 
 # Optimize controller bundle
 wasm-opt -Oz --enable-bulk-memory -o ./pkg-controller/account_wasm_bg.wasm ./pkg-controller/account_wasm_bg.wasm
 
 # Build session bundle
 RUSTFLAGS='-C link-arg=-s -C opt-level=z -C codegen-units=1' \
-wasm-pack build --target bundler --out-dir ./pkg-session --release --features "session_account,wee_alloc"
+    wasm-pack build --target bundler --out-dir ./pkg-session --out-name session_wasm --release --features "session_account,wee_alloc"
 
 # Optimize session bundle
-wasm-opt -Oz --enable-bulk-memory -o ./pkg-session/account_wasm_bg.wasm ./pkg-session/account_wasm_bg.wasm
+wasm-opt -Oz --enable-bulk-memory -o ./pkg-session/session_wasm_bg.wasm ./pkg-session/session_wasm_bg.wasm
