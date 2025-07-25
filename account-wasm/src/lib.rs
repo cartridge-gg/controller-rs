2fbe1a93f986316e11179da656170e49d8f0f3dd
tedison <timothyedison@hotmail.com>
2025-07-25 13:42:27 +0200

    Fix is_safari boolean + compilation error

--- account-wasm/src/lib.rs
+++ account-wasm/src/lib.rs
@@ -3,12 +3,14 @@ mod alloc;
 #[cfg(feature = "controller_account")]
 pub mod account;
 
+#[cfg(feature = "controller_account")]
+mod owner;
+
 #[cfg(feature = "session_account")]
 pub mod session;
 
 mod errors;
 
-mod owner;
 mod storage;
 mod sync;
 mod types;
