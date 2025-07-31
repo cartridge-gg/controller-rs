a8db1da0b20868adfd73bfbed4e5707369f466ea
Tarrence van As <tarrence@vanas.family>
2025-07-31 16:16:53 -0400

    Scale price estimates by 2x

--- account_sdk/src/controller.rs
+++ account_sdk/src/controller.rs
@@ -197,7 +197,7 @@ impl Controller {
             .execute_v3(calls.clone())
             .nonce(Felt::from(u64::MAX))
             .gas_estimate_multiplier(1.5)
-            .gas_price_estimate_multiplier(1.5)
+            .gas_price_estimate_multiplier(2.0)
             .estimate_fee()
             .await;
 
